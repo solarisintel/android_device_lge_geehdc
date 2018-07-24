d54e818838f33f330aab6bbf2a516f3844a14d69
SolarisIntel <labo@dev-VirtualBox.(none)>
2018-07-24 13:15:23 +0900

    first commit

---
+++ rr.mk
@@ -0,0 +1,16 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/geehdc/aosp_geehdc.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := rr_geehdc
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := Optimus G
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geehdc_dcm_jp BUILD_FINGERPRINT=lge/geehdc_dcm_jp/geehdc:4.0.4/IMM76L/.1363085524:user/release-keys
