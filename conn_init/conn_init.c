671cbbd5762aec7851061b5ab5c5c2e5bb2ee272
solarisintel <solarisintel@yahoo.co.jp>
2020-01-16 09:02:14 +0900

    cm-12.0のソースに戻す。WifiのMACアドレス取得はこちらが正しい

--- conn_init/conn_init.c
+++ conn_init/conn_init.c
@@ -17,7 +17,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <cutils/log.h>
-
 extern int wfc_util_qcom_check_config(unsigned char *nv_mac_addr);
 extern void wfc_util_atoh(char *pAsciiString, int szAsciiString, unsigned char *pHexaBuff, int szHexaBuff);
 
@@ -25,49 +24,32 @@ static int wifi_check_qcom_cfg_files()
 {
     char macAddress[13];
     char hex[7];
-    memset(macAddress, 0, sizeof(macAddress));
+    memset(macAddress, 0, 13);
     memset(hex, 0, 7);
 
     // Read MAC String
     FILE *fp = NULL;
     int n = 0;
-    fp = fopen("/persist/wifi/.macaddr", "r"); //Nexus 4 location
+    fp = fopen("/dev/block/platform/msm_sdcc.1/by-name/misc", "r");
     if ( fp == NULL )
     {
-        ALOGW("Failed to open /persist/wifi/.macaddr for reading. Failing over to misc partition.");
-        fp = fopen("/dev/block/platform/msm_sdcc.1/by-name/misc", "r"); //Optimus G location
-        if ( fp == NULL )
-        {
-            ALOGE("Failed to open misc partition file for wlan macaddr reading");
-        
-            wfc_util_qcom_check_config((unsigned char *)macAddress);
-            return 0;
-        }
-        else
-        {
-            unsigned char macbuf[6];
-            fseek(fp,0x3000,SEEK_SET);
-            n = fread(macbuf, 6, 1, fp);
-            sprintf(macAddress,"%02x%02x%02x%02x%02x%02x",
-                    macbuf[0], macbuf[1], macbuf[2],
-                    macbuf[3], macbuf[4], macbuf[5]);
-            fclose(fp);
-        }
+        ALOGD("Failed to open NV for wlan macaddr read");
+        return -1;
     }
     else
     {
-        n = fread(macAddress, 12, 1, fp);
+        unsigned char macbuf[6];
+        fseek(fp,0x3000,SEEK_SET);
+        n = fread(macbuf, 6, 1, fp);
+        sprintf(macAddress,"%02x%02x%02x%02x%02x%02x",
+                macbuf[0], macbuf[1], macbuf[2],
+                macbuf[3], macbuf[4], macbuf[5]);
         fclose(fp);
-        if (n == 0) {
-            // Buffer may be partially written. Reset.
-            memset(macAddress, 0, sizeof(macAddress));
-        }
+
+        // Write MAC String
+        wfc_util_atoh( macAddress, 12, (unsigned char *)hex, 6);
+        wfc_util_qcom_check_config((unsigned char *)hex);
     }
-
-    // Write MAC String
-    wfc_util_atoh( macAddress, 12, (unsigned char *)hex, 6);
-    wfc_util_qcom_check_config((unsigned char *)hex);
-
     return 1;
 }
 
@@ -77,3 +59,4 @@ int main(void)
 
     return 0;
 }
+
